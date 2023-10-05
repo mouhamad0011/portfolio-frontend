783b87dafacd9b1c32b07cec4a91305f36088049
Mhmd Sleimane <mohammad.sleimane@isae.edu.lb>
2023-10-05 14:27:18 +0300

    ..

--- HomePage/home-page/src/Experience.js
+++ HomePage/home-page/src/Experience.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import styles from'./style.module.css';
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons'; 
+import styles from './style.module.css';
+
 function Experience() {
   const [experienceItems, setExperienceItems] = useState([]);
   const [newPosition, setNewPosition] = useState('');
   const [newDescription, setNewDescription] = useState('');
   const [newDate, setNewDate] = useState('');
   const [newPlace, setNewPlace] = useState('');
-  const [infos, setInfos] = useState(true);
+  const [editingItemId, setEditingItemId] = useState(null); // State for the item being edited
+  const [infos,setInfos]=useState(true);
+
 
   useEffect(() => {
     fetch('http://localhost:5000/experience/getAll')
@@ -55,6 +58,56 @@ function Experience() {
     }
   };
 
+  const handleEditExperience = async () => {
+    if (editingItemId) {
+      const editedExperienceItem = {
+        position: newPosition,
+        date: newDate,
+        place: newPlace,
+        description: newDescription,
+      };
+      await fetch(`http://localhost:5000/experience/update/${editingItemId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editedExperienceItem),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const updatedExperienceItems = experienceItems.map((item) =>
+            item._id === editingItemId ? data : item
+          );
+          setExperienceItems(updatedExperienceItems);
+          setEditingItemId(null);
+          setNewPosition('');
+          setNewDate('');
+          setNewPlace('');
+          setNewDescription('');
+          setInfos(!infos);
+        })
+        .catch((error) => console.error('Error editing experience:', error));
+    }
+  };
+
+  const startEditing = (id) => {
+    setEditingItemId(id);
+    const itemToEdit = experienceItems.find((item) => item._id === id);
+    if (itemToEdit) {
+      setNewPosition(itemToEdit.position);
+      setNewDate(itemToEdit.date);
+      setNewPlace(itemToEdit.place);
+      setNewDescription(itemToEdit.description);
+    }
+  };
+
+  const cancelEditing = () => {
+    setEditingItemId(null);
+    setNewPosition('');
+    setNewDate('');
+    setNewPlace('');
+    setNewDescription('');
+  };
   const handleDeleteExperience = async (id) => {
     try {
       await fetch(`http://localhost:5000/experience/delete/${id}`, {
@@ -71,6 +124,7 @@ function Experience() {
             (item) => item._id !== id
           );
           setExperienceItems(updatedExperienceItems);
+          setInfos(!infos);
         })
         .catch((error) => {
           console.error('Error deleting experience:', error);
@@ -79,58 +133,94 @@ function Experience() {
       console.error('Error deleting experience:', error);
     }
   };
-
+  
   return (
     <div className={styles.Experience}>
       <h1>Experiences</h1>
-      <ul id={styles["experience-list"]}>
+      <ul id={styles['experience-list']}>
         {experienceItems.map((item) => (
-          <li key={item._id}> 
+          <li key={item._id}>
             {item.position}, {item.date}, {item.place}
             <FontAwesomeIcon
               icon={faTrashAlt}
               className="fa-lg delete"
               onClick={() => handleDeleteExperience(item._id)}
             />
+            <FontAwesomeIcon
+              icon={faEdit}
+              className="fa-lg edit"
+              onClick={() => startEditing(item._id)}
+            />
           </li>
         ))}
       </ul>
-      <h2>Add New Experience</h2>
-      <form id={styles["add-experience-form"]} onSubmit={handleAddExperience}>
+      <h2>{editingItemId ? 'Edit Experience' : 'Add New Experience'}</h2>
+      <form id={styles['add-experience-form']} onSubmit={handleAddExperience}>
         <input
           type="text"
           placeholder="Position"
           value={newPosition}
           onChange={(e) => setNewPosition(e.target.value)}
-        /><br/>
+        />
+        <br />
         <textarea
           name="description"
           placeholder="Description"
           value={newDescription}
           onChange={(e) => setNewDescription(e.target.value)}
-        ></textarea><br/>
+        ></textarea>
+        <br />
         <input
           type="text"
           placeholder="Date"
           value={newDate}
           onChange={(e) => setNewDate(e.target.value)}
-        /><br/>
+        />
+        <br />
         <input
           type="text"
           placeholder="Place"
           value={newPlace}
           onChange={(e) => setNewPlace(e.target.value)}
         />
-        <button
-          type="submit"
-          style={{
-            padding: '5px 10px',
-            fontSize: '14px',
-            width: '80px',
-          }}
-        >
-          Add
-        </button>
+        {editingItemId ? (
+          <>
+            <button
+              type="button"
+              onClick={handleEditExperience}
+              style={{
+                padding: '5px 10px',
+                fontSize: '14px',
+                width: '80px',
+                marginRight: '10px',
+              }}
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              onClick={cancelEditing}
+              style={{
+                padding: '5px 10px',
+                fontSize: '14px',
+                width: '80px',
+              }}
+            >
+              Cancel
+            </button>
+          </>
+        ) : (
+          <button
+            type="submit"
+            style={{
+              padding: '5px 10px',
+              fontSize: '14px',
+              width: '80px',
+            }}
+          >
+            Add
+          </button>
+        )}
       </form>
     </div>
   );
--- HomePage/home-page/src/Projects.js
+++ HomePage/home-page/src/Projects.js
@@ -1,137 +1,314 @@
-import React, { useState, useEffect ,useRef} from 'react';
-import axios from 'axios';
-import styles from "./style.module.css";
+import React, { useState, useEffect, useRef } from 'react';
+import styles from './style.module.css';
 
 function Projects() {
-  const [projectName, setProjectName] = useState('');
-  const [projectDescription, setProjectDescription] = useState('');
-  const [projectLink, setProjectLink] = useState('');
-  const [infos, setInfos] = useState(true);
-  const [image,setImage]=useState('');
   const [projects, setProjects] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newDescription, setNewDescription] = useState('');
+  const [newLink, setNewLink] = useState('');
+  const [editingProjectId, setEditingProjectId] = useState(null);
+  const [infos, setInfos] = useState(true);
+  const [image, setImage] = useState('');
   const fileInput = useRef(null);
 
   useEffect(() => {
     fetch('http://localhost:5000/projects/getAll')
-      .then((response) => response.json())
-      .then((data) => setProjects(data.data))
-      .catch((error) => console.error('Error fetching projects:', error));
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('error');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProjects(data.data);
+      })
+      .catch(() => {
+        console.error('Error in fetching');
+      });
   }, [infos]);
 
   const handleFileChange = (e) => {
     setImage(e.target.files[0]);
   };
 
-
   const handleAddProject = async (e) => {
     e.preventDefault();
+
     const formData = new FormData();
     formData.append('image', image);
 
     try {
-      const response = await axios.post(
+      const response = await fetch(
         'https://api.imgbb.com/1/upload?key=61433a8c98f2a424d6ab481eca2fb4a0',
-        formData
-      );
-      const imageUrl = response.data.data.url;
-      setImage(imageUrl);
-      console.log('Image uploaded successfully:', imageUrl);
-      try {
-        const response2 = await fetch(`http://localhost:5000/projects/add`, {
+        {
           method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            name:projectName,
-            image: imageUrl,
-            description: projectDescription,
-            link:projectLink
-          }),
-        });
-      
-        if (!response2.ok) {
-          throw new Error(`errorrr`);
+          body: formData,
         }
-        const responseData = await response2.json();
-        console.log(responseData);
-        setInfos(!infos);
-      } catch (error) {
-        console.error(error);
+      );
+
+      if (!response.ok) {
+        throw new Error('Image upload failed');
       }
-      
+
+      const imageUrl = (await response.json()).data.url;
+
+      const newProject = {
+        name: newName,
+        image: imageUrl,
+        description: newDescription,
+        link: newLink,
+      };
+
+      await fetch('http://localhost:5000/projects/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newProject),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setProjects([...projects, data]);
+          setNewName('');
+          setNewDescription('');
+          setNewLink('');
+          setImage('');
+          setInfos(!infos);
+        })
+        .catch((error) => console.error('Error adding project:', error));
     } catch (error) {
-      console.error('Error:', error);
+      console.error('Error uploading image:', error);
     }
   };
-  
-  const handleDeleteProject = (projectId) => {
-    fetch(`http://localhost:5000/projects/delete/${projectId}`, {
-      method: 'DELETE',
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        const updatedProjects = projects.filter((project) => project._id !== data._id);
-        setProjects(updatedProjects);
-        setInfos(!infos);
+
+  const handleEditProject = async () => {
+    if (editingProjectId) {
+      let editedImageURL = projects.find(
+        (project) => project._id === editingProjectId
+      ).image;
+
+      if (image) {
+        const formData = new FormData();
+        formData.append('image', image);
+
+        try {
+          const response = await fetch(
+            'https://api.imgbb.com/1/upload?key=61433a8c98f2a424d6ab481eca2fb4a0',
+            {
+              method: 'POST',
+              body: formData,
+            }
+          );
+
+          if (response.ok) {
+            editedImageURL = (await response.json()).data.url;
+            console.log(editedImageURL);
+          } else {
+            throw new Error('Image upload failed');
+          }
+        } catch (error) {
+          console.error('Error uploading image:', error);
+        }
+      }
+
+      const editedProject = {
+        name: newName,
+        description: newDescription,
+        link: newLink,
+        image: editedImageURL
+      };
+
+      await fetch(`http://localhost:5000/projects/update/${editingProjectId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(editedProject),
       })
-      .catch((error) => console.error('Error deleting project:', error));
+        .then((response) => response.json())
+        .then((data) => {
+          const updatedProjects = projects.map((project) =>
+            project._id === editingProjectId ? data : project
+          );
+          setProjects(updatedProjects);
+          setEditingProjectId(null);
+          setNewName('');
+          setNewDescription('');
+          setNewLink('');
+          setImage('');
+          setInfos(!infos);
+        })
+        .catch((error) => console.error('Error editing project:', error));
+    }
+  };
+
+  const startEditing = (id) => {
+    setEditingProjectId(id);
+    const projectToEdit = projects.find((project) => project._id === id);
+    if (projectToEdit) {
+      setNewName(projectToEdit.name);
+      setNewDescription(projectToEdit.description);
+      setNewLink(projectToEdit.link);
+    }
+  };
+
+  const cancelEditing = () => {
+    setEditingProjectId(null);
+    setNewName('');
+    setNewDescription('');
+    setNewLink('');
+    setImage('');
+  };
+
+  const handleDeleteProject = async (id) => {
+    try {
+      await fetch(`http://localhost:5000/projects/delete/${id}`, {
+        method: 'DELETE',
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(() => {
+          const updatedProjects = projects.filter((project) => project._id !== id);
+          setProjects(updatedProjects);
+          setInfos(!infos);
+        })
+        .catch((error) => {
+          console.error('Error deleting project:', error);
+        });
+    } catch (error) {
+      console.error('Error deleting project:', error);
+    }
   };
 
   return (
     <div className={styles.Projects}>
       <h1>Projects</h1>
-      <ul id={styles["projects-list"]}>
-        {projects && projects.map((project) => (
+      <ul id={styles['projects-list']}>
+        {projects.map((project) => (
           <li key={project._id}>
-            <div>
-              <h3>{project.name}<a href={project.link}>{project.link}</a></h3>
-              <p>{project.description}</p>
-            </div>
-            <button
-              onClick={() => handleDeleteProject(project._id)}
-              style={{
-                padding: '5px 10px',
-                fontSize: '14px',
-                width: '80px',
-              }}
-            >
-              Delete
-            </button>
+            {editingProjectId === project._id ? (
+              <>
+                <input
+                  type="text"
+                  placeholder="Project Name"
+                  value={newName}
+                  onChange={(e) => setNewName(e.target.value)}
+                />
+                <br />
+                <input
+                  type="text"
+                  placeholder="Project Link"
+                  value={newLink}
+                  onChange={(e) => setNewLink(e.target.value)}
+                />
+                <br />
+                <input
+                  type="file"
+                  name="image"
+                  onChange={handleFileChange}
+                  ref={fileInput}
+                />
+                <br />
+                <textarea
+                  name="description"
+                  placeholder="Project Description"
+                  value={newDescription}
+                  onChange={(e) => setNewDescription(e.target.value)}
+                ></textarea>
+                <br />
+                <button
+                  type="button"
+                  onClick={handleEditProject}
+                  style={{
+                    padding: '5px 10px',
+                    fontSize: '14px',
+                    width: '80px',
+                    marginRight: '10px',
+                  }}
+                >
+                  Save
+                </button>
+                <button
+                  type="button"
+                  onClick={cancelEditing}
+                  style={{
+                    padding: '5px 10px',
+                    fontSize: '14px',
+                    width: '80px',
+                  }}
+                >
+                  Cancel
+                </button>
+              </>
+            ) : (
+              <>
+                <div>
+                  <h3>
+                    {project.name}
+                    <a href={project.link}>{project.link}</a>
+                  </h3>
+                  <p>{project.description}</p>
+                </div>
+                <button
+                  onClick={() => handleDeleteProject(project._id)}
+                  style={{
+                    padding: '5px 10px',
+                    fontSize: '14px',
+                    width: '80px',
+                  }}
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={() => startEditing(project._id)}
+                  style={{
+                    padding: '5px 10px',
+                    fontSize: '14px',
+                    width: '80px',
+                  }}
+                >
+                  Edit
+                </button>
+              </>
+            )}
           </li>
         ))}
       </ul>
-  
-      <h2>Add New Project</h2>
-      <form id={styles["add-project-form"]} onSubmit={handleAddProject}>
+
+      <h2>{editingProjectId ? 'Edit Project' : 'Add New Project'}</h2>
+      <form id={styles['add-project-form']} onSubmit={handleAddProject}>
         <input
           type="text"
           placeholder="Project Name"
-          id={styles["project-name"]}
-          value={projectName}
-          onChange={(e) => setProjectName(e.target.value)}
-        /><br/>
-        <input
-          type="file"
-          name="image"
-          id={styles["project-image"]}
-          onChange={handleFileChange}
-          ref={fileInput}
-        /><br/>
-        <textarea
-          name="description"
-          id={styles["project-description"]}
-          placeholder="Project Description"
-          value={projectDescription}
-          onChange={(e) => setProjectDescription(e.target.value)}
-        ></textarea><br/>
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+        />
+        <br />
         <input
           type="text"
           placeholder="Project Link"
-          id={styles["project-link"]}
-          value={projectLink}
-          onChange={(e) => setProjectLink(e.target.value)}
-        /><br/>
+          value={newLink}
+          onChange={(e) => setNewLink(e.target.value)}
+        />
+        <br />
+        <input
+          type="file"
+          name="image"
+          onChange={handleFileChange}
+          ref={fileInput}
+        />
+        <br />
+        <textarea
+          name="description"
+          placeholder="Project Description"
+          value={newDescription}
+          onChange={(e) => setNewDescription(e.target.value)}
+        ></textarea>
+        <br />
         <button
           type="submit"
           style={{
@@ -145,7 +322,6 @@ function Projects() {
       </form>
     </div>
   );
-  
 }
 
 export default Projects;
